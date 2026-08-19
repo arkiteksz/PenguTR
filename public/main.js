bf3a4b294eb842fa3e49f70ef732119faed3b44a
arkiteksz <barisgk63@gmail.com>
2026-08-19 21:22:50 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -303,9 +303,9 @@ const SKIN_COLORS = { blue: '#3d6fb0', green: '#3d8f4a', black: '#2a2a2a', purpl
 // width: gorselin ekranda kac piksel genislikte cizilecegi (oran korunur). Ince ayar icin bu sayilari degistir.
 const WEAPON_SPRITES = {
   pistol:  { src: '/assets/weapons/pistol.png',  offsetX: 0, offsetY: -28, width: 30 },
-  smg:     { src: '/assets/weapons/smg.png',     offsetX: 0, offsetY: -28, width: 60 },
+  smg:     { src: '/assets/weapons/smg.png',     offsetX: -10, offsetY: -45, width: 60 },
   shotgun: { src: '/assets/weapons/shotgun.png', offsetX: -20, offsetY: -26, width: 70 },
-  sniper:  { src: '/assets/weapons/sniper.png',  offsetX: -33, offsetY: -30, width: 80 },
+  sniper:  { src: '/assets/weapons/sniper.png',  offsetX: -23, offsetY: -30, width: 80 },
   rocket:  { src: '/assets/weapons/rocket.png',  offsetX: -33, offsetY: -36, width: 80 },
   grenade: { src: '/assets/weapons/grenade.png', offsetX: 8,  offsetY: -34, width: 16 },
 };
